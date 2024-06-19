867e789bbaf67ce3e2dffa819da4ec4c28d21dec
Noah Paulssen <noah.paulssen@fressnapf.com>
2024-06-19 13:13:40 +0200

    integrating fontend json with flask route

--- app/app.py
+++ app/app.py
@@ -1,20 +1,28 @@
 import mysql.connector
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json
 
-# connect to mariaDB
+## connect to mariaDB in xampp container
 #db = mysql.connector.connect(
-#    host="localhost",
+#    host="127.0.0.1",
 #    user="root",
 #    password="password"
 #)
-#
-## print(db)
+
+# print(db)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/query", methods=['GET','POST'])
+def query():
+    with open('home.json') as json_data:
+        d = jsonify(json_data)
+    return d
+    
 
 
 if __name__ == "__main__":
